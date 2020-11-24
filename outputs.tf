53a0472aade77ad4d0caec50f1ddc0b98bb151b5
jakemulley <jakemulley@users.noreply.github.com>
2020-11-24 16:02:54 +0000

    Use key for output, rather than account.name

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "environment_account_ids" {
   sensitive = true
   value = {
-    for account in aws_organizations_account.accounts :
-    account.name => account.id
+    for key, account in aws_organizations_account.accounts :
+    key => account.id
   }
 }
